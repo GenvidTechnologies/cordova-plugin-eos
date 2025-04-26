c405d8d9fab214bc884cc32a4d089a828a3b61bb
Fabien Ninoles <fninoles@genvidtech.com>
2025-04-26 13:08:50 -0400

    BUR-0000: Fix linting

--- demo/www/js/index.js
+++ demo/www/js/index.js
@@ -21,7 +21,7 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
+function onDeviceReady () {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
