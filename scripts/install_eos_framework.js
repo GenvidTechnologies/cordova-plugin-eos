689523ee641b1e079b9c78f8b169868a486e77de
Fabien Ninoles <fninoles@genvidtech.com>
2025-05-15 19:42:11 +0000

    Merged in BUR-6049-EOSSDK-Info-plist-patch (pull request #20)
    BUR-6049 EOSSDK Info plist patch
    
    * BUR-6049: Add patch from epic
    
    * BUR-6049: Update version to 1.2.1
    
    * BUR-6049: Fixed lint
    
    * BUR-6049: Used import for scripts

--- scripts/install_eos_framework.js
+++ scripts/install_eos_framework.js
@@ -8,6 +8,10 @@ function installEOSFramework (context) {
     console.log(`Copying ${framework}`);
     const destination = path.join(context.opts.plugin.pluginInfo.dir, 'src/ios/EOSSDK.framework');
     fs.cpSync(framework, destination, { recursive: true, force: true });
+    // Applied SDK patch
+    // https://eoshelp.epicgames.com/s/article/When-running-a-test-flight-for-iOS-why-am-I-running-into-EOSSDK-framework-does-not-support-the-minimum-OS-Version-specified-in-the-Info-plist
+    const pInfo = path.join(__dirname, 'EOSSDK-Info.plist');
+    fs.cpSync(pInfo, path.join(destination, 'Info.plist'));
 }
 
 module.exports = installEOSFramework;
